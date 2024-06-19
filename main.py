9a0c7ba06bddc1472503b6302fa6c604f607775e
kaufman79 <kaufman79@gmail.com>
2024-06-19 10:55:24 -0400

    add subheader above plot, made option_underscore variables to simplify code

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ y_option = st.selectbox("Select the data for the Y-axis",
 st.write("(Note that a lower 'corruption' score indicates more corruption, "
          "whereas a higher score indicates less corruption)")
 
-x_option = x_option.replace(" ", "_")
-y_option = y_option.replace(" ", "_")
+x_option_underscore = x_option.replace(" ", "_")
+y_option_underscore = y_option.replace(" ", "_")
 
 df = pd.read_csv("happy.csv")
-x_data = df[x_option]
-y_data = df[y_option]
+x_data = df[x_option_underscore]
+y_data = df[y_option_underscore]
 
+st.subheader(f"{x_option} and {y_option}".title())
 figure = px.scatter(x=x_data, y=y_data,
-                    labels={"x": x_option.replace("_", " "), "y": y_option.replace("_", " ")})
+                    labels={"x": x_option, "y": y_option})
 st.plotly_chart(figure)
